e42ac1528da163c384d28364aca0eae9dd979563
chris roubideaux <51684535+chrisroubideaux@users.noreply.github.com>
2024-08-07 23:52:58 -0500

    updated utils folder

--- components/sales/starters/StarterWeeklyChart.js
+++ components/sales/starters/StarterWeeklyChart.js
@@ -14,7 +14,7 @@ import {
   Legend,
 } from 'chart.js';
 import ChartDataLabels from 'chartjs-plugin-datalabels';
-import { startOfWeek, endOfWeek, format } from 'date-fns';
+import { startOfWeek, endOfWeek, format, getDay } from 'date-fns';
 
 ChartJS.register(
   CategoryScale,
@@ -34,8 +34,9 @@ const formatNumber = (number) => {
   }
   return number.toString();
 };
-const generateWeeklySalesData = (weeklyTotal) => {
-  const days = 6; // Number of days in the week
+
+// Function to generate weekly sales data with adjustments
+const generateWeeklySalesData = (weeklyTotal, days = 7) => {
   const dailyAverage = weeklyTotal / days;
   return Array.from({ length: days }, () => {
     const variance = faker.datatype.float({
@@ -51,80 +52,9 @@ const currentWeekSalesTotal = 8500; // 8.5k
 const previousWeekSalesTotal = 8900; // 8.9k
 const weeklyAverageSales = 8800; // 8.8k
 
-// Generate current and previous weekly sales data
-const currentWeekSales = generateWeeklySalesData(currentWeekSalesTotal);
-const previousWeekSales = generateWeeklySalesData(previousWeekSalesTotal);
-const averageData = generateWeeklySalesData(weeklyAverageSales);
-
-const lineChartOptions = {
-  responsive: true,
-  plugins: {
-    legend: {
-      position: 'top',
-    },
-    title: {
-      display: true,
-      text: 'Weekly Sales Data',
-    },
-    tooltip: {
-      callbacks: {
-        label: function (context) {
-          return `$${formatNumber(context.raw)}`;
-        },
-      },
-    },
-    datalabels: {
-      anchor: 'end',
-      align: 'end',
-      formatter: function (value) {
-        return formatNumber(value);
-      },
-      color: 'black',
-      font: {
-        weight: 'normal',
-      },
-    },
-  },
-  scales: {
-    y: {
-      ticks: {
-        callback: function (value) {
-          return `$${formatNumber(value)}`;
-        },
-      },
-    },
-  },
-};
-
-const lineChartData = {
-  labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
-  datasets: [
-    {
-      label: `Current Week $${formatNumber(currentWeekSalesTotal)}`,
-      data: currentWeekSales,
-      borderColor: 'rgb(177, 188, 255)',
-      backgroundColor: 'rgba(177, 188, 255, 0.5)',
-      fill: false,
-    },
-    {
-      label: `Previous Week $${formatNumber(previousWeekSalesTotal)}`,
-      data: previousWeekSales,
-      borderColor: 'rgba(53, 162, 235, 0.5)',
-      backgroundColor: 'rgba(53, 162, 235, 0.5)',
-      fill: false,
-    },
-    {
-      label: `Average $${formatNumber(weeklyAverageSales)}`,
-      data: averageData,
-      borderColor: 'rgb(255, 205, 86)',
-      backgroundColor: 'rgba(255, 205, 86, 0.5)',
-      fill: false,
-    },
-  ],
-};
-
 export default function StarterWeeklyChart({ setActiveComponent }) {
   const [currentWeek, setCurrentWeek] = useState('');
+  const [currentDay, setCurrentDay] = useState(new Date().getDay()); // Get current day (0=Sunday, 1=Monday, ..., 6=Saturday)
 
   useEffect(() => {
     const now = new Date();
@@ -134,6 +64,89 @@ export default function StarterWeeklyChart({ setActiveComponent }) {
     const formattedEnd = format(end, 'MM/dd/yyyy');
     setCurrentWeek(`${formattedStart} - ${formattedEnd}`);
   }, []);
+
+  // Generate weekly sales data
+  const allDaysSales = generateWeeklySalesData(currentWeekSalesTotal);
+  const currentWeekSales = allDaysSales.slice(0, currentDay + 1); // Slice data to include up to current day
+  const previousWeekSales = generateWeeklySalesData(previousWeekSalesTotal);
+  const averageData = generateWeeklySalesData(weeklyAverageSales);
+
+  const lineChartOptions = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: 'Weekly Sales Data',
+      },
+      tooltip: {
+        callbacks: {
+          label: function (context) {
+            return `$${formatNumber(context.raw)}`;
+          },
+        },
+      },
+      datalabels: {
+        anchor: 'end',
+        align: 'end',
+        formatter: function (value) {
+          return formatNumber(value);
+        },
+        color: 'black',
+        font: {
+          weight: 'normal',
+        },
+      },
+    },
+    scales: {
+      y: {
+        ticks: {
+          callback: function (value) {
+            return `$${formatNumber(value)}`;
+          },
+        },
+      },
+    },
+  };
+
+  const lineChartData = {
+    labels: [
+      'Monday',
+      'Tuesday',
+      'Wednesday',
+      'Thursday',
+      'Friday',
+      'Saturday',
+    ],
+    datasets: [
+      {
+        label: `Current Week $${formatNumber(currentWeekSalesTotal)}`,
+        data: [
+          ...currentWeekSales,
+          ...Array(6 - currentWeekSales.length).fill(null),
+        ], // Fill the rest with null to avoid display issues
+        borderColor: 'rgb(177, 188, 255)',
+        backgroundColor: 'rgba(177, 188, 255, 0.5)',
+        fill: false,
+      },
+      {
+        label: `Previous Week $${formatNumber(previousWeekSalesTotal)}`,
+        data: previousWeekSales,
+        borderColor: 'rgba(53, 162, 235, 0.5)',
+        backgroundColor: 'rgba(53, 162, 235, 0.5)',
+        fill: false,
+      },
+      {
+        label: `Average $${formatNumber(weeklyAverageSales)}`,
+        data: averageData,
+        borderColor: 'rgb(255, 205, 86)',
+        backgroundColor: 'rgba(255, 205, 86, 0.5)',
+        fill: false,
+      },
+    ],
+  };
 
   return (
     <div className="container-fluid">
@@ -180,7 +193,7 @@ import {
   Legend,
 } from 'chart.js';
 import ChartDataLabels from 'chartjs-plugin-datalabels';
-import { startOfWeek, endOfWeek, format } from 'date-fns';
+import { startOfWeek, endOfWeek, format, getDay } from 'date-fns';
 
 ChartJS.register(
   CategoryScale,
@@ -200,15 +213,21 @@ const formatNumber = (number) => {
   }
   return number.toString();
 };
-const generateWeeklySalesData = (weeklyTotal) => {
+
+// Function to generate weekly sales data with adjustments for the current week
+const generateWeeklySalesData = (weeklyTotal, currentDay) => {
   const days = 6; // Number of days in the week
   const dailyAverage = weeklyTotal / days;
-  return Array.from({ length: days }, () => {
+
+  // Create data with adjusted values for days that have not yet occurred
+  return Array.from({ length: days }, (_, index) => {
+    const dayOfWeek = (index + 1) % 7; // Mapping index to day of week (Monday is 1, Sunday is 0)
+    const isFutureDay = dayOfWeek > currentDay;
     const variance = faker.datatype.float({
       min: -dailyAverage * 0.2,
       max: dailyAverage * 0.2,
     }); // Random variance +/- 20% of daily average
-    return dailyAverage + variance;
+    return isFutureDay ? dailyAverage * 0.5 : dailyAverage + variance;
   });
 };
 
@@ -217,80 +236,9 @@ const currentWeekSalesTotal = 8500; // 8.5k
 const previousWeekSalesTotal = 8900; // 8.9k
 const weeklyAverageSales = 8800; // 8.8k
 
-// Generate current and previous weekly sales data
-const currentWeekSales = generateWeeklySalesData(currentWeekSalesTotal);
-const previousWeekSales = generateWeeklySalesData(previousWeekSalesTotal);
-const averageData = generateWeeklySalesData(weeklyAverageSales);
-
-const lineChartOptions = {
-  responsive: true,
-  plugins: {
-    legend: {
-      position: 'top',
-    },
-    title: {
-      display: true,
-      text: 'Weekly Sales Data',
-    },
-    tooltip: {
-      callbacks: {
-        label: function (context) {
-          return `$${formatNumber(context.raw)}`;
-        },
-      },
-    },
-    datalabels: {
-      anchor: 'end',
-      align: 'end',
-      formatter: function (value) {
-        return formatNumber(value);
-      },
-      color: 'black',
-      font: {
-        weight: 'normal',
-      },
-    },
-  },
-  scales: {
-    y: {
-      ticks: {
-        callback: function (value) {
-          return `$${formatNumber(value)}`;
-        },
-      },
-    },
-  },
-};
-
-const lineChartData = {
-  labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
-  datasets: [
-    {
-      label: `Current Week $${formatNumber(currentWeekSalesTotal)}`,
-      data: currentWeekSales,
-      borderColor: 'rgb(177, 188, 255)',
-      backgroundColor: 'rgba(177, 188, 255, 0.5)',
-      fill: false,
-    },
-    {
-      label: `Previous Week $${formatNumber(previousWeekSalesTotal)}`,
-      data: previousWeekSales,
-      borderColor: 'rgba(53, 162, 235, 0.5)',
-      backgroundColor: 'rgba(53, 162, 235, 0.5)',
-      fill: false,
-    },
-    {
-      label: `Average $${formatNumber(weeklyAverageSales)}`,
-      data: averageData,
-      borderColor: 'rgb(255, 205, 86)',
-      backgroundColor: 'rgba(255, 205, 86, 0.5)',
-      fill: false,
-    },
-  ],
-};
-
 export default function StarterWeeklyChart({ setActiveComponent }) {
   const [currentWeek, setCurrentWeek] = useState('');
+  const [currentDay, setCurrentDay] = useState(new Date().getDay()); // Get current day (0=Sunday, 1=Monday, ..., 6=Saturday)
 
   useEffect(() => {
     const now = new Date();
@@ -300,6 +248,91 @@ export default function StarterWeeklyChart({ setActiveComponent }) {
     const formattedEnd = format(end, 'MM/dd/yyyy');
     setCurrentWeek(`${formattedStart} - ${formattedEnd}`);
   }, []);
+
+  // Generate weekly sales data based on current day
+  const currentWeekSales = generateWeeklySalesData(
+    currentWeekSalesTotal,
+    currentDay
+  );
+  const previousWeekSales = generateWeeklySalesData(
+    previousWeekSalesTotal,
+    currentDay
+  );
+  const averageData = generateWeeklySalesData(weeklyAverageSales, currentDay);
+
+  const lineChartOptions = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: 'Weekly Sales Data',
+      },
+      tooltip: {
+        callbacks: {
+          label: function (context) {
+            return `$${formatNumber(context.raw)}`;
+          },
+        },
+      },
+      datalabels: {
+        anchor: 'end',
+        align: 'end',
+        formatter: function (value) {
+          return formatNumber(value);
+        },
+        color: 'black',
+        font: {
+          weight: 'normal',
+        },
+      },
+    },
+    scales: {
+      y: {
+        ticks: {
+          callback: function (value) {
+            return `$${formatNumber(value)}`;
+          },
+        },
+      },
+    },
+  };
+
+  const lineChartData = {
+    labels: [
+      'Monday',
+      'Tuesday',
+      'Wednesday',
+      'Thursday',
+      'Friday',
+      'Saturday',
+    ],
+    datasets: [
+      {
+        label: `Current Week $${formatNumber(currentWeekSalesTotal)}`,
+        data: currentWeekSales,
+        borderColor: 'rgb(177, 188, 255)',
+        backgroundColor: 'rgba(177, 188, 255, 0.5)',
+        fill: false,
+      },
+      {
+        label: `Previous Week $${formatNumber(previousWeekSalesTotal)}`,
+        data: previousWeekSales,
+        borderColor: 'rgba(53, 162, 235, 0.5)',
+        backgroundColor: 'rgba(53, 162, 235, 0.5)',
+        fill: false,
+      },
+      {
+        label: `Average $${formatNumber(weeklyAverageSales)}`,
+        data: averageData,
+        borderColor: 'rgb(255, 205, 86)',
+        backgroundColor: 'rgba(255, 205, 86, 0.5)',
+        fill: false,
+      },
+    ],
+  };
 
   return (
     <div className="container-fluid">
@@ -328,6 +361,7 @@ export default function StarterWeeklyChart({ setActiveComponent }) {
     </div>
   );
 }
+
 
 */
 }
