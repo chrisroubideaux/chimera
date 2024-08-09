e0dc93ccfb78279a88a99f21c1f91278739dc4ed
chris roubideaux <51684535+chrisroubideaux@users.noreply.github.com>
2024-08-09 11:53:56 -0500

    updated sales folder

--- components/sales/entrees/EntreeMonthlyChart.js
+++ components/sales/entrees/EntreeMonthlyChart.js
@@ -13,6 +13,150 @@ import {
 import ChartDataLabels from 'chartjs-plugin-datalabels';
 import { format } from 'date-fns';
 import Nav from './Nav';
+import { faker } from '@faker-js/faker';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  ChartDataLabels
+);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top',
+    },
+    datalabels: {
+      display: true,
+      align: 'end',
+      anchor: 'end',
+      formatter: (value) => {
+        const formattedValue = Math.round(value / 1000); // Round to the nearest whole number
+        return `${formattedValue}k`;
+      },
+    },
+  },
+  scales: {
+    y: {
+      ticks: {
+        callback: function (value) {
+          return `${Math.round(value / 1000)}k`; // Round y-axis ticks to the nearest whole number
+        },
+      },
+    },
+  },
+};
+
+const labels = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+];
+
+const generateMonthlyData = () => {
+  return labels.map((_, index) => {
+    if (index === 7) {
+      // For August, considering the current date
+      const currentDate = new Date();
+      const currentDay = currentDate.getDate();
+      const averageDailySales = faker.datatype.float({
+        min: 3000,
+        max: 3500,
+        precision: 0.01,
+      });
+      return Math.round(averageDailySales * currentDay); // Sales so far in the month
+    }
+    return Math.round(
+      faker.datatype.float({ min: 90000, max: 105000, precision: 0.01 })
+    ); // Random sales data for other months
+  });
+};
+
+const averageMonthlySales = 97500; // Average of $90,000 and $105,000
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: 'Projected',
+      data: generateMonthlyData(),
+      borderColor: 'rgb(126, 142, 241)',
+      backgroundColor: 'rgb(177, 188, 255)',
+    },
+    {
+      label: 'Actual',
+      data: generateMonthlyData(),
+      borderColor: 'rgb(53, 162, 235)',
+      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+    },
+    {
+      label: 'Average',
+      data: Array(labels.length).fill(averageMonthlySales),
+      borderColor: 'rgb(255, 159, 64)',
+      backgroundColor: 'rgba(255, 159, 64, 0.5)',
+    },
+  ],
+};
+
+export default function EntreeMonthlyChart({ setActiveComponent }) {
+  const [currentDate, setCurrentDate] = useState('');
+
+  useEffect(() => {
+    const now = new Date();
+    const formattedDate = format(now, 'MM/dd/yyyy');
+    setCurrentDate(formattedDate);
+  }, []);
+
+  return (
+    <div className="container-fluid">
+      <div className="card">
+        <div className="card-body">
+          <div className="row mb-3">
+            <div className="col-md-6 col-xl-4 mb-2 mb-md-0">
+              <span className="d-flex">
+                <h5 className="mb-0 me-1">Entrees:</h5>
+                <p>{currentDate}</p>
+              </span>
+            </div>
+            <div className="col-md-6 col-xl-8">
+              <div className="d-flex justify-content-end">
+                <Nav setActiveComponent={setActiveComponent} />
+              </div>
+            </div>
+          </div>
+          <Bar className="" options={options} data={data} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+{
+  /*
+import { useState, useEffect } from 'react';
+import { Bar } from 'react-chartjs-2';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import ChartDataLabels from 'chartjs-plugin-datalabels';
+import { format } from 'date-fns';
+import Nav from './Nav';
 
 ChartJS.register(
   CategoryScale,
@@ -107,4 +251,7 @@ export default function EntreeMonthlyChart({ setActiveComponent }) {
       </div>
     </div>
   );
+}
+
+*/
 }
--- components/sales/entrees/EntreeWeeklyChart.js
+++ components/sales/entrees/EntreeWeeklyChart.js
@@ -13,6 +13,150 @@ import {
 } from 'chart.js';
 import { startOfWeek, endOfWeek, format } from 'date-fns';
 import Nav from './Nav';
+import { faker } from '@faker-js/faker';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+// Function to generate random weekly sales data
+const generateWeeklySalesData = (min, max) => {
+  return Array.from({ length: 6 }, () =>
+    faker.datatype.float({ min, max, precision: 0.1 })
+  );
+};
+
+// Generate weekly sales data
+const currentWeekData = generateWeeklySalesData(21, 24.5); // Adjust the range if needed
+const previousWeekData = generateWeeklySalesData(21, 24.5); // Adjust the range if needed
+
+const averageSales = (data) =>
+  data.reduce((acc, val) => acc + val, 0) / data.length;
+
+const lineChartOptions = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top',
+    },
+    title: {
+      display: true,
+    },
+    tooltip: {
+      callbacks: {
+        label: function (context) {
+          return `${context.raw}k`;
+        },
+      },
+    },
+  },
+  scales: {
+    y: {
+      ticks: {
+        callback: function (value) {
+          return `${value}k`;
+        },
+      },
+    },
+  },
+};
+
+const labels = [
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+];
+
+const lineChartData = {
+  labels,
+  datasets: [
+    {
+      label: 'Current Week',
+      data: currentWeekData,
+      borderColor: 'rgb(177, 188, 255)',
+      backgroundColor: 'rgb(177, 188, 255)',
+    },
+    {
+      label: 'Previous Week',
+      data: previousWeekData,
+      borderColor: 'rgba(53, 162, 235, 0.5)',
+      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+    },
+    {
+      label: 'Average',
+      data: Array(labels.length).fill(averageSales(currentWeekData).toFixed(1)),
+      borderColor: 'rgb(255, 159, 64)',
+      backgroundColor: 'rgba(255, 159, 64, 0.5)',
+      borderDash: [],
+    },
+  ],
+};
+
+export default function EntreeWeeklyChart({ setActiveComponent }) {
+  const [currentWeek, setCurrentWeek] = useState('');
+
+  useEffect(() => {
+    const now = new Date();
+    const start = startOfWeek(now, { weekStartsOn: 1 });
+    const end = endOfWeek(now, { weekStartsOn: 1 });
+    const formattedStart = format(start, 'MM/dd/yyyy');
+    const formattedEnd = format(end, 'MM/dd/yyyy');
+    setCurrentWeek(`${formattedStart} - ${formattedEnd}`);
+  }, []);
+
+  return (
+    <div className="container-fluid">
+      <div className="card">
+        <div className="card-body">
+          <div className="row mb-3">
+            <div className="col-md-6 col-xl-4 mb-2 mb-md-0">
+              <div className="d-flex ">
+                <h5 className="mb-0 me-1">Entrees:</h5>
+                <p className="mb-0">{currentWeek}</p>
+              </div>
+            </div>
+            <div className="col-md-6 col-xl-8">
+              <div className="d-flex justify-content-end">
+                <Nav setActiveComponent={setActiveComponent} />
+              </div>
+            </div>
+          </div>
+          <Line
+            className="my-2"
+            options={lineChartOptions}
+            data={lineChartData}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+{
+  /*
+import { useState, useEffect } from 'react';
+import { Line } from 'react-chartjs-2';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { startOfWeek, endOfWeek, format } from 'date-fns';
+import Nav from './Nav';
 
 ChartJS.register(
   CategoryScale,
@@ -108,4 +252,7 @@ export default function EntreeWeeklyChart({ setActiveComponent }) {
       </div>
     </div>
   );
+}
+
+*/
 }
