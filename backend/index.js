a8bdd0f780091e5f9d52166a95540deb8dd7d53e
chris roubideaux <51684535+chrisroubideaux@users.noreply.github.com>
2024-12-17 19:56:02 -0600

    update nextConfig.js file

--- backend/index.js
+++ backend/index.js
@@ -60,6 +60,23 @@ app.use(
 );
 
 // Session setup with connect-mongo
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGO_URI,
+    }),
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24, // 1 day
+    },
+  })
+);
+
+{
+  /*
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
@@ -71,6 +88,8 @@ app.use(
     },
   })
 );
+*/
+}
 
 // Body parsing middleware
 app.use(json());
