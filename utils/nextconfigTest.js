1358ff9ac73c2c2dbc136bf463b7bba01e299a5f
chris roubideaux <51684535+chrisroubideaux@users.noreply.github.com>
2024-12-19 20:45:05 -0600

    updated menu page

--- utils/nextconfigTest.js
+++ utils/nextconfigTest.js
@@ -1,17 +1,8 @@
-const path = require('path');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
   env: {
     NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
-  },
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@': path.resolve(__dirname),
-    };
-    return config;
   },
 };
 
