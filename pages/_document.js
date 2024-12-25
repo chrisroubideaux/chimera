2a0369103e493fbb69cfb8bf7096caea9a452585
chris roubideaux <51684535+chrisroubideaux@users.noreply.github.com>
2024-12-25 10:54:48 -0600

    added product, inventory dashboard pages

--- pages/_document.js
+++ pages/_document.js
@@ -1,3 +1,35 @@
+// pages/_document.js
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossorigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Assistant:wght@200..800&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          rel="stylesheet"
+          href="https://use.fontawesome.com/releases/v6.1.1/css/all.css"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
+
+{
+  /*
 import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
@@ -29,28 +61,5 @@ export default function Document() {
   );
 }
 
-{
-  /*
-import { Html, Head, Main, NextScript } from "next/document";
-
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head>
-      <link rel="preconnect" href="https://fonts.googleapis.com"/>
-      <link rel="preconnect" href="https://fonts.gstatic.com"/>
-      <link href="https://fonts.googleapis.com/css2?family=Assistant:wght@200..800&display=swap" rel="stylesheet"></link>
-      <link
-          rel="stylesheet"
-          href="https://use.fontawesome.com/releases/v6.1.1/css/all.css"
-        />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
-}
 */
 }
