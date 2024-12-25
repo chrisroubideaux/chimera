228577b70d09bad70b87ccdef403c17b1c63b194
chris roubideaux <51684535+chrisroubideaux@users.noreply.github.com>
2024-12-25 10:30:01 -0600

    added product, inventory dashboard pages

--- pages/inventory/[id].js
+++ pages/inventory/[id].js
@@ -1,3 +1,5 @@
+{
+  /*
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import axios from 'axios';
@@ -174,11 +176,5 @@ export default function Inventory() {
     </>
   );
 }
-
-{
-  /*
-
-    {renderComponent()}
-
 */
 }
